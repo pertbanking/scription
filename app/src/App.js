9c9bce1b4c49838a5bd4a6b0d2e179e6a7f5f71e
Josh Petrin <pertbanking@gmail.com>
2024-05-28 20:09:05 -0500

    adding the react app and some images that I need to redo

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo-750.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" width="200" height="200" />
+        <p>
+          Welcome to Scription!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
