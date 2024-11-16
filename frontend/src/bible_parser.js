d537efe0a7e47d28be52cbeb60412c37042e7f80
Josh Petrin <13423755+pertbanking@users.noreply.github.com>
2024-11-16 14:09:26 -0600

    add bible_parser.js

---
+++ frontend/src/bible_parser.js
@@ -0,0 +1,36 @@
+// -*- encoding: utf-8 -*-
+//
+// (c) Joshua Petrin 2024. All rights reserved.
+//
+// File creation date: 15 November 2024
+// File creator: Joshua Petrin
+
+'use strict';
+
+// Parse the XML Bibles in the 'bible' folder
+const SRC_FOLDER = "bible/";
+
+// class Bible {
+//     constructor(version) {
+//         this.data = 
+//     }
+// };
+
+const importVerse = (book, chapter, verse) => {
+    let entire_bible_str;
+    // fs.readFile(SRC_FOLDER + "esv.xml", "utf8", (err, data) => {
+    //     if (err) { throw err; }
+    //     else { entire_bible_str = data; }
+    // });
+
+    // let desired_verse;
+
+    // console.log(parseString);
+
+    // parseString(entire_bible_str, (err, data) => {
+    //     if (err) { throw err; }
+    //     else { console.log(data); }
+    // });
+}
+
+export default importVerse;
---
+++ frontend/src/tests/bible_parser.test.js
@@ -0,0 +1,24 @@
+// -*- encoding: utf-8 -*-
+//
+// (c) Joshua Petrin 2024. All rights reserved.
+//
+// File creation date: 15 November 2024
+// File creator: Joshua Petrin
+
+/// Test the Bible parser (make sure it doesn't commit any heresy)
+
+import importVerse from '../bible_parser';
+
+test('john first verse', () => {
+    let expected = "In the beginning was the word, and the word was with God, and the word was God.";
+
+    expect(importVerse('john', 1, 1)).toBe(expected);
+    expect(importVerse('John', 1, 1)).toBe(expected);
+    expect(importVerse('jOhN', 1, 1)).toBe(expected);
+});
+
+test('john second verse', () => {
+    let expected = "He was in the beginning with God.";
+
+    expect(importVerse('john', 1, 2)).toBe(expected);
+})
