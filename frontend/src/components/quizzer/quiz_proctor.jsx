a70af4c1cd3ac75592622d877e1624847cd21831
Josh Petrin <13423755+pertbanking@users.noreply.github.com>
2024-11-16 14:09:43 -0600

    add quiz proctor

---
+++ frontend/src/components/quizzer/quiz_proctor.jsx
@@ -0,0 +1,18 @@
+// -*- encoding: utf-8 -*-
+//
+// (c) Joshua Petrin 2024. All rights reserved.
+//
+// File creation date: 15 November 2024
+// File creator: Joshua Petrin
+
+import React, { useState } from 'react';
+
+const trimText = text => {
+    
+};
+
+const QuizProctor = ({text}) => {
+
+};
+
+export default QuizProctor;
