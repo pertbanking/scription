ae589032d5a756af3db5567553e40277e54f69bb
Josh Petrin <13423755+pertbanking@users.noreply.github.com>
2025-07-18 22:31:15 -0500

    started writing tests for the text

--- frontend/src/tests/app.test.jsx
+++ frontend/src/tests/app.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../app";
 
-test.skip("renders learn react link", () => {
+test.skip("render whole app", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
---
+++ frontend/src/tests/quiz_text.test.jsx
@@ -0,0 +1,33 @@
+// -*- encoding: utf-8 -*-
+//
+// (c) Joshua Petrin 2025. All rights reserved.
+//
+// File creation date: 18 July 2025
+// File creator: Joshua Petrin
+
+import { render, screen } from "@testing-library/react";
+import QuizText from "../components/quizzer/quiz_text";
+
+test("render text", () => {
+  let text = "a fine shotgun";
+  render(<QuizText text={text} userGuesses={"afs"} />);
+  const linkElement = screen.getByText(/shotgun/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("render incorrect guess", () => {
+  let text = "a fine blueberry";
+  render(<QuizText text={text} userGuesses={"n"} />);
+  const linkElement = screen.getByText("a");
+  expect(linkElement).toBeInTheDocument();
+  expect(linkElement.className).toMatch(/has-text-danger/);
+});
+
+test("only render guessed words", () => {
+  let text = "a fine blueberry";
+  render(<QuizText text={text} userGuesses={"ad"} />);
+  const linkElement = screen.queryByText(/blueberry/);
+  expect(linkElement).toBeNull();
+});
+
+test.todo("find a way to add in annotations and verse numbers");
