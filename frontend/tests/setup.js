fcacfb0e0dcd3bb9c8c122b3e41b6c5c0c4c9a00
Josh Petrin <13423755+pertbanking@users.noreply.github.com>
2025-08-02 14:49:50 -0500

    add a mention

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -9,6 +9,9 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 
+// Credit to Corina Udrescu, who provided me with this setup file:
+// https://reactpractice.dev/articles/simple-react-and-vite-setup-with-unit-testing/
+
 expect.extend(matchers);
 
 afterEach(() => {
